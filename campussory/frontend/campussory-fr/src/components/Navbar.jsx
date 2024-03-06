62c8b17a08f59a517557cc2a49e1cd36db080170
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-03-07 02:35:13 +0530

    new start

--- campussory/frontend/campussory-fr/src/components/Navbar.jsx
+++ campussory/frontend/campussory-fr/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '/src/Styles/Navbar.css'
 import img1 from '/public/images/WhatsApp Image 2024-03-07 at 12.25.22 AM.jpeg'
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className='nav'>
@@ -9,9 +10,10 @@ function Navbar() {
           <img src={img1} alt="" />
         </div>
         <div id="nav_right">
-          <a href="">Home</a>
-          <a href="">My Schedules</a>
-          <a href="">Prep Materials</a>
+          <Link to = "/">Home</Link>
+          <Link to = "/demo">My Schedules</Link>
+          <Link to = "/prep">Prep Materials</Link>
+          
         </div>
       </div>
     </div>
